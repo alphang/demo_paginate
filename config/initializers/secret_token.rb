b62255113c849fe483e95d8aa7777d943ebb8d66
alphang <alphang@server.fake>
2013-03-26 13:50:43 -0700

    First commit. Post model exists with three attributes. Faker and Kaminari are implemented.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoPaginate::Application.config.secret_token = '32d3a73f944c69a5fb2a883861c51d080a9268337b4e0b6ee3f63f95da586e770c6367393e610a0dbe64a5ce3a773c36daac752961a70fd1cca646d69a74adc8'
